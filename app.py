1ef18a89e9c05396c513751f15d416f6cea293fb
Azamat Zakirovch <161879875+azamatzakirovch18@users.noreply.github.com>
2024-11-12 23:58:44 +0500

    Update app.py
    erf

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ st.set_page_config(
     layout="wide"           
 )
 
-# Load custom CSS to hide footer and icons
-# with open("style.css") as f:
-    # st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-# The rest of your Streamlit app code follows here
+with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
 
 
 
